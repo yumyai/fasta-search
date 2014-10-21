6bc9b2c5e7ae1077ffaadcfd00bf166b85016f83
Preecha Patumcharoenpol <yumyai@gmail.com>
2014-10-21 15:10:02 +0700

    Add test template

--- src/test/scala/api/GeneServiceSpec.scala
+++ src/test/scala/api/GeneServiceSpec.scala
@@ -3,6 +3,20 @@ package api
 /**
  * Created by preecha on 10/16/14 AD.
  */
-class GeneServiceSpec {
+import spray.testkit.Specs2RouteTest
+import spray.routing.Directives
+import org.specs2.mutable.Specification
+import spray.http.HttpResponse
+
+class GeneServiceSpec extends Specification with Directives with Specs2RouteTest {
+  "The routing infrastructure should support" >> {
+    "the most simple and direct route" in {
+      Post() ~> complete(HttpResponse()) ~> (_.response) === HttpResponse()
+    }
+
+    "Another" in {
+      Post() ~> complete(HttpResponse()) ~> (_.response) === HttpResponse()
+    }
+  }
 
 }
--- src/test/scala/core/GeneActorSpec.scala
+++ src/test/scala/core/GeneActorSpec.scala
@@ -1,8 +1,24 @@
 package core
 
+
+import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.ActorSystem
+
+import org.specs2.mutable.SpecificationLike
+import java.util.UUID
+
 /**
  * Created by preecha on 10/16/14 AD.
  */
-class GeneActorSpec {
+class GeneActorSpec extends TestKit(ActorSystem()) with SpecificationLike with CoreActors with Core with ImplicitSender {
 
+  import GeneActor._
+
+  "Gene should" >> {
+
+    "reject invalid email" in {
+      gene ! BatchSymbolQuery(List(""))
+      success
+    }
+  }
 }
